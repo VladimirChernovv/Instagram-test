7e25c14ce9c3325c2261c1aa160f44e0325562d1
Vladimir Chernov <v.chernov@zebrains.team>
2021-05-11 17:31:37 +0300

    Завершили работу над компонентом header. Реализовали переход к регистрации и авторизации по кнопкам Log in, Sign up

--- src/seed.js
+++ src/seed.js
@@ -1,8 +1,8 @@
-// NOTE: replace 'sQzJ58smbaQY0eM4u5i5aRwNoub2' with your Firebase auth user id (can be taken from Firebase)
+// NOTE: replace '6QykGFA7bYdQrBLwyem7NIkhmpb2' with your Firebase auth user id (can be taken from Firebase)
 export function seedDatabase(firebase) {
   const users = [
     {
-      userId: 'sQzJ58smbaQY0eM4u5i5aRwNoub2',
+      userId: '6QykGFA7bYdQrBLwyem7NIkhmpb2',
       username: 'karl',
       fullName: 'Karl Hadwen',
       emailAddress: 'karlhadwen@gmail.com',
@@ -16,7 +16,7 @@ export function seedDatabase(firebase) {
       fullName: 'Raffaello Sanzio da Urbino',
       emailAddress: 'raphael@sanzio.com',
       following: [],
-      followers: ['sQzJ58smbaQY0eM4u5i5aRwNoub2'],
+      followers: ['6QykGFA7bYdQrBLwyem7NIkhmpb2'],
       dateCreated: Date.now()
     },
     {
@@ -25,7 +25,7 @@ export function seedDatabase(firebase) {
       fullName: 'Salvador Dalí',
       emailAddress: 'salvador@dali.com',
       following: [],
-      followers: ['sQzJ58smbaQY0eM4u5i5aRwNoub2'],
+      followers: ['6QykGFA7bYdQrBLwyem7NIkhmpb2'],
       dateCreated: Date.now()
     },
     {
@@ -34,7 +34,7 @@ export function seedDatabase(firebase) {
       fullName: 'George Orwell',
       emailAddress: 'george@orwell.com',
       following: [],
-      followers: ['sQzJ58smbaQY0eM4u5i5aRwNoub2'],
+      followers: ['6QykGFA7bYdQrBLwyem7NIkhmpb2'],
       dateCreated: Date.now()
     }
   ];
