ac1d2d003caacc42d9a93aa3ec207965bd148a92
Vladimir Chernov <v.chernov@zebrains.team>
2021-05-11 19:20:25 +0300

    Реализовали новый хук user-users.js. Начали разрабатывать компонент sidebar

---
+++ src/components/sidebar/suggestions.js
@@ -0,0 +1,5 @@
+export default function Suggestions() {
+  return (
+    <p>We are the suggestions</p>
+  );
+};
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import {useEffecet, useEffect} from 'react';
+import {useEffect} from 'react';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Timeline from '../components/timeline';
@@ -11,7 +11,7 @@ export default function Dashboard() {
   return (
     <div className="bg-gray-background">
       <Header />
-      <div className="grid">
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
         <Timeline />
         <Sidebar /> 
       </div>
