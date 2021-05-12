5729dd84b52fe5b243d6fd46ff73b181a901113f
Vladimir Chernov <v.chernov@zebrains.team>
2021-05-12 13:26:36 +0300

    Начали использовать свойство для провекри пропсов propTYpes в компоненте user.js. Продолжаем работу с компонентом user.js

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,5 +1,30 @@
-export default function User() {
-  return (
-    <p>I am a user</p>
+import {memo} from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+const User = ({username, fullName}) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link className="grid grid-cols-4 gap-4 mb-6 items-center" to={`/p/${username}`}>
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
   );
+
+export default memo(User);
+
+User.propsTypes = {
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
 };
