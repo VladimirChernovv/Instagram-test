57044f62f0c3a2465b4941aed53ec984afff0730
Vladimir Chernov <v.chernov@zebrains.team>
2021-05-13 14:29:45 +0300

    Создали компонент Post и в нём создаём новый компонент Header. Используем PropTypes для вывода данных пользователя в посте.

---
+++ src/components/post/index.js
@@ -0,0 +1,27 @@
+import {useRef} from "react";
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({content}) {
+  // components
+  // -> we need header, image, actions (like & comment icons), footer, comments
+
+  return (
+    <div className="rounded col-span-4 border bg-white border-white border-gray-primary mb-16">
+      <Header username={content.username} />
+    </div>
+  );
+};
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  })
+};
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,6 @@
 import Skeleton from 'react-loading-skeleton';
-import usePhotos from '../hooks/use-photos'; 
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 export default function Timeline() {
   // we need to get the logged in user's photos (hook)
@@ -12,13 +13,9 @@ export default function Timeline() {
   return (
     <div className="container col-span-2">
       {!photos ? (
-        <>
-          {[...new Array(4)].map((_, index) =>
-            <Skeleton className="mb-5" count={4} width={640} height={500} />
-          )}
-        </>
+        <Skeleton className="mb-5" count={4} width={640} height={500} />
       ) : photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
         <p className="text-center text-2xl">Follow people to see photos!</p>
       )}
