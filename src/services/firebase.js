85e0fcdc3730c9fcb5b790e10084febc96196b2a
Vladimir Chernov <v.chernov@zebrains.team>
2021-05-17 18:00:30 +0300

    1) We created the isUserFollowingProfile function in services / firebase, which transfers all information about the user on the page. 2) Implemented the display of the header with the presence of information (the number of photos / subscriptions / subscribers). 3) Implemented a dynamic display of the number of subscribers (when the subscriber is deleted, the information also changes) and when the page is refreshed, the information about the number of subscribers and the button label remain correct. 4) Created a new component component / profile / photos

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,3 @@
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import {firebase, FieldValue} from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
@@ -9,6 +8,21 @@ export async function doesUsernameExist(username) {
     .get();
 
   return result.docs.map((user) => user.data().length > 0);
+};
+
+export async function getUserByUsername(username) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', username)
+    .get();
+
+  const user = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return user.length > 0 ? user : false;
 };
 
 // get user from the firestore where userId === userId (pass from the auth)
@@ -99,4 +113,54 @@ export async function getPhotos(userId, following) {
   );
 
   return photosWithUserDetails;
+};
+
+export async function getUserPhotosByUsername(username) {
+  const [user] = await getUserByUsername(username);
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', '==', user.userId)
+    .get();
+
+  return (
+    result.docs.map((item) => ({
+      ...item.data(),
+      docId: item.id,
+    }))
+  );
+};
+
+export async function isUserFollowingProfile(loggedInUserUsername, profileUserId) {
+  const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('username', '==', loggedInUserUsername) // karl (active logged in user)
+    .where('following', 'array-contains', profileUserId)
+    .get();
+
+  const [response = {}] = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return response.userId;
+};
+
+export async function toggleFollow(
+  isFollowingProfile,
+  activeUserDocId,
+  profileDocId,
+  profileUserId,
+  followingUserId,
+) {
+  // 1st param: karl's doc id
+  // 2nd param: raphael's user id
+  // 3rd param: is the user following this profile? e.g. does karl follow raphael ? (true/false)
+  await updateLoggedInUserFollowing(activeUserDocId, profileUserId, isFollowingProfile);
+
+  // 1st param: karl's user id
+  // 2nd param: raphael's doc id
+  // 3rd param: is the user following this profile? e.g. does karl follow raphael ? (true/false)
+  await updateFollowedUserFollowers(profileDocId, followingUserId, isFollowingProfile);
 };
